b94ee87e458d2865235357530fa56dba240b3e96
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2017-02-16 12:34:16 -0500

    Adding cue functionality

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var iframe = document.querySelector('iframe');
+var player = new Vimeo.Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
