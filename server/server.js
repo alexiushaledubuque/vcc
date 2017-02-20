2b90ab3c5f21fd77599770a63b968556ee8c2276
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2017-02-19 19:57:16 -0500

    Adding server & create dbs

--- server/server.js
+++ server/server.js
@@ -1,12 +1,67 @@
-var path = require('path');
+/**
+	*	Required Middleware	
+	*/
+
 var express = require('express');
+var path = require('path');
+
+/**
+	*	Express Server
+	*/
+console.log('STARTING BACKEND ===>');
 
 var app = express();
+module.exports = app;
+
+/**
+	*	Database Server - Adding Persistence
+	*/
+console.log('STARTING THE DBS SERVER - CREATING THE TABLES');
+
+var db = require('knex')({
+	client: 'sqlite3',
+	connection: {
+		filename: 'db/video-storage.db'
+	},
+	useNullAsDefault: true
+});
+
+db.schema.hasTable('Videos').then(function(exists){
+	if (!exists){
+		db.schema.createTableIfNotExists('Videos', function(table){
+			table.increments();
+			table.string('video-id');
+			table.string('title');
+		}).then(function(){
+			console.log('Created Videos table');
+		});		
+	};	
+});
+
+// db.schema.hasTable('VideoCues').then(function(exists){
+// 	if (!exists){
+// 		db.schema.createTableIfNotExists('VideoCues', function(table){
+// 			table.increments();
+// 			table.string('cue-id');
+// 			table.number('cue-pt');
+// 			table.string('cue-msg');
+// 			table.string('video-id')
+// 		}).then(function(){
+// 			console.log('Created videos table');
+// 		});		
+// 	};	
+// });
+
 var port = 3000;
 
 var staticPath = path.join(__dirname, '../');
 
+/**
+	*	Serve my static files
+	*/
 app.use(express.static(staticPath));
+
+
 
 app.listen(port, function() {
   console.log('listening on port: ', port);
